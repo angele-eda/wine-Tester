e0fa98f2fa21edf2cac8f4f4f3c43e6d2f658248
angele-eda <1004dayu@gmail.com>
2026-07-13 22:53:16 +0900

    Add files via upload

---
+++ assets/cf24-modern.js
@@ -0,0 +1,6 @@
+(function(){
+  var bar=document.createElement('header');
+  bar.className='cf24-bar';
+  bar.innerHTML='<a class="cf24-brand" href="/"><span class="cf24-brand-mark">C</span><span>ConvertFiles<b>24</b></span></a><nav class="cf24-nav"><a href="/">모든 도구</a><a href="/privacy/index.html">개인정보 보호</a></nav>';
+  document.body.insertBefore(bar,document.body.firstChild);
+})();
